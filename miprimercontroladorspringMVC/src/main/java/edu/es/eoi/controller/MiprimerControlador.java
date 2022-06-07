2c6e8d5d138ca1963d1b431d004dd740e032bbdd
jj <jotajota1302@hotmail.com>
2022-06-07 16:57:12 +0200

    commit

--- miprimercontroladorspringMVC/src/main/java/edu/es/eoi/controller/MiprimerControlador.java
+++ miprimercontroladorspringMVC/src/main/java/edu/es/eoi/controller/MiprimerControlador.java
@@ -1,21 +1,40 @@
 package edu.es.eoi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/client")
 public class MiprimerControlador {
 		
-	@RequestMapping(method = RequestMethod.GET)
-	public String getHello() {
+	@RequestMapping(method = RequestMethod.GET)	
+	public String getHelloHtml() {
 		
-		System.out.println("Alguien me ha llamado");
-		
-		//si hay que llamar aun servicio lo llamo
+		//recuperar todos los alumnos
 		
 		return "hello.html";
+	}
+	
+	@RequestMapping(value = "/dni",method = RequestMethod.GET)
+	@ResponseBody
+	public String getDniByRequest(@RequestParam String nombre) {		
+		
+		System.out.println("Voy a buscar el dni de: " + nombre);
+		
+		return "483612738A";
+	}
+	
+	@RequestMapping(value = "/dni/{nombre}",method = RequestMethod.GET)
+	@ResponseBody
+	public String getDniByPathPariable(@PathVariable String nombre) {		
+		
+		System.out.println("Voy a buscar el dni de: " + nombre);
+		
+		return "483612738A";
 	}
 
 }
