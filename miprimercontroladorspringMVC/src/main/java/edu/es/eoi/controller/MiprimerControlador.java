01b00ac8cb2b7eed0ace1383c573d8888432bda7
jj <jotajota1302@hotmail.com>
2022-06-07 19:36:08 +0200

    commit

--- miprimercontroladorspringMVC/src/main/java/edu/es/eoi/controller/MiprimerControlador.java
+++ miprimercontroladorspringMVC/src/main/java/edu/es/eoi/controller/MiprimerControlador.java
@@ -1,10 +1,13 @@
 package edu.es.eoi.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,15 +19,15 @@ import edu.es.eoi.domain.Cliente;
 @RequestMapping("/cliente")
 public class MiprimerControlador {
 
+//	@RequestMapping(method = RequestMethod.GET)
+//	public String getHelloHtml() {
+//
+//		// recuperar todos los clientes
+//
+//		return "hello.html";
+//	}
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String getHelloHtml() {
-
-		// recuperar todos los clientes
-
-		return "hello.html";
-	}
-
-	@RequestMapping(value = "/todos", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Cliente> getAllClients() {
 
@@ -35,17 +38,43 @@ public class MiprimerControlador {
 
 		return lista;
 	}
-
-	@RequestMapping(value = "/dni", method = RequestMethod.GET)
+	
+	@RequestMapping(method = RequestMethod.DELETE)
 	@ResponseBody
-	public String getDniByRequest(@RequestParam String nombre) {
-
-		System.out.println("Voy a buscar el dni de: " + nombre);
-
-		return "483612738A";
+	public String deleteAll(@Nullable @RequestParam String usuario,@Nullable @RequestParam String pw) {
+		
+		if(usuario!=null&&usuario.equals("JJ")) {
+			return "borro todos los clientes";
+		}else {
+			return "no permitido";
+		}
+	
 	}
 
-	@RequestMapping(value = "/dni/{nombre}", method = RequestMethod.GET)
+	@RequestMapping(value = "/hora", method = RequestMethod.GET)
+	@ResponseBody
+	public LocalDateTime getTime() {		
+		return LocalDateTime.now();
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public String createCliente(@RequestBody @Nullable Cliente cliente) {		
+		
+		//recoger los campos de cliente y insertarlos en la bbdd
+		
+		if(cliente==null) {
+			System.out.println("faltan los datos del cliente");
+			return "falta info";
+		}else {
+			System.out.println("inserto un cliente nuevo en bbdd");			
+			return "Cliente creado";
+		}
+	}
+
+
+	@RequestMapping(value = "/{nombre}", method = RequestMethod.GET)
 	@ResponseBody
 	public String getDniByPathPariable(@PathVariable String nombre) {
 
