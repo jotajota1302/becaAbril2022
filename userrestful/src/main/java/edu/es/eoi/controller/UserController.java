15c74be241ff5fca5147091ffbd35960fc258e39
jj <jotajota1302@hotmail.com>
2022-06-08 19:12:49 +0200

    commit

--- userrestful/src/main/java/edu/es/eoi/controller/UserController.java
+++ userrestful/src/main/java/edu/es/eoi/controller/UserController.java
@@ -1,6 +1,8 @@
 package edu.es.eoi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,52 +19,50 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
-	public User find(@PathVariable String id) {
-		
-		try {		
-			int pk=Integer.valueOf(id);
-			return service.find(pk);
-			
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable String id) {
+
+		try {
+			int pk = Integer.valueOf(id);
+			return new ResponseEntity<>(service.find(pk), HttpStatus.OK);
+
 		} catch (Exception e) {
-			System.out.println("id con formato incorrecto");
-			return null;
-		}		
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public void create(@RequestBody User user) {	
-		
-		if(user.getId()!=0) {
-			System.out.println("usuario ya existente");	
-		}else {
-			service.create(user);
-		}		
-		
-	}
-	
-	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
-	public void delete(@PathVariable String id) {	
-		
-		User user=service.find(Integer.valueOf(id));
-		
-		service.delete(user);
-		
-	}
-	
-	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
-	public void update(@PathVariable String id, @RequestBody User user) {	
-		
-		if(user.getId()!=Integer.valueOf(id)) {
-			System.out.println("request incorrecta");
-		}else {
-			service.update(user);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-		
-		
-		
 	}
 
-	
-	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> create(@RequestBody User user) {
+
+		if (user.getId() != 0) {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		} else {
+			service.create(user);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		}
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> delete(@PathVariable String id) {
+
+		User user = service.find(Integer.valueOf(id));
+		service.delete(user);
+
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<?> update(@PathVariable String id, @RequestBody User user) {
+
+		if (user.getId() != Integer.valueOf(id)) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} else {
+			service.update(user);
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		}
+
+	}
+
 }
