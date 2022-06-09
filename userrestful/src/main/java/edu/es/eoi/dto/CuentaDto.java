f99f4ea3be912156cbd037f400bdc42879ef45dd
jj <jotajota1302@hotmail.com>
2022-06-09 16:42:03 +0200

    commit

---
+++ userrestful/src/main/java/edu/es/eoi/dto/CuentaDto.java
@@ -0,0 +1,38 @@
+package edu.es.eoi.dto;
+
+public class CuentaDto {
+	
+	private String dni;
+	
+	private double saldo;
+	
+	private String banco;
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+	
+	
+	
+
+}
