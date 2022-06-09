d27165e2afc45820c584ba1ec04cc25ccf01377d
jj <jotajota1302@hotmail.com>
2022-06-09 18:18:57 +0200

    commit

---
+++ springdataexample/src/main/java/edu/es/eoi/User.java
@@ -0,0 +1,61 @@
+package edu.es.eoi;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "beca", name = "usuarios")
+public class User {
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nombre")
+	private String username;
+	
+	@Column
+	private String mail;
+	
+	@Column
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
--- springdataexample/src/test/java/edu/es/eoi/SpringDataExampleApplicationTests.java
+++ springdataexample/src/test/java/edu/es/eoi/SpringDataExampleApplicationTests.java
@@ -1,13 +1,21 @@
 package edu.es.eoi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringDataExampleApplicationTests {
 
+	@Autowired
+	UserRepository repository;
+	
 	@Test
 	void contextLoads() {
+		
+		System.out.println(repository.findAll());
+		
+		
 	}
 
 }
