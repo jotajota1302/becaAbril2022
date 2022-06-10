cbb4ada051af73ac29bcfe42f0963fddfab92b77
jj <jotajota1302@hotmail.com>
2022-06-10 15:56:09 +0200

    commit

---
+++ userrestful/src/main/java/edu/es/eoi/dto/UserDto.java
@@ -0,0 +1,28 @@
+package edu.es.eoi.dto;
+
+public class UserDto {
+
+	private String username;
+
+	private String lastLogin;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}	
+
+	public String getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(String lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	
+	
+
+}
